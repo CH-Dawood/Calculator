7305bd751105dddbe9950168dc0dd25ca10d7ac6
M.Dawood Arif <ch1dawood178@gmail.com>
2024-03-31 19:44:00 +0500

    first commit

---
+++ Script.js
@@ -0,0 +1,9 @@
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       // inline JavaScript
